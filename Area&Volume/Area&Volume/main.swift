8e55c96b9e893367489d670399fe4f483e4efadc
tuwiqBootcamp <abdullah.ali.ruh.s@tuwaiq.edu.sa>
2021-10-11 21:24:57 +0300

    Done

--- Area&Volume/Area&Volume/main.swift
+++ Area&Volume/Area&Volume/main.swift
@@ -7,5 +7,9 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Enter a radius:", terminator: "")
+var radius = Double(readLine()!)
+var area = Double.pi * pow(radius!,2)
+var volume = 4 * Double.pi * pow(radius!,3) / 3
 
+print("Area is: \(String(describing: Double(String(format: "%.0f", area))!)) and Volume is: \(String(describing: Double(String(format: "%.0f", volume))!))")
