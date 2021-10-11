aafa0083751b2cecd116520383198a0db6dc312c
Lola M <Lola@MacBook-Pro.local>
2021-10-11 21:17:01 +0300

    This is my day2 hw

---
+++ circle radius/circle radius/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  circle radius
+//
+//  Created by Lola M on 10/11/21.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ circle radius/circle radius/utils.swift
@@ -0,0 +1,8 @@
+//
+//  utils.swift
+//  circle radius
+//
+//  Created by Lola M on 10/11/21.
+//
+
+import Foundation
