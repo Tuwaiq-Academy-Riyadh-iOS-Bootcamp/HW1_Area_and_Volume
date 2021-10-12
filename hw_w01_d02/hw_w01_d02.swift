020cc86656c4a41f3a73f0e7e0a335f19e40c0fb
esho <esho@MacBook-Air-esho.local>
2021-10-12 09:03:53 +0300

    eshraq

---
+++ hw_w01_d02/hw_w01_d02.swift
@@ -0,0 +1,15 @@
+//
+//  hw_w01_d02.swift
+//  hw_w01_d02
+//
+//  Created by esho on 06/03/1443 AH.
+//
+
+import Foundation
+
+
+//print("enter radius :")
+//let r = Utils.readDouble()
+//let area = round(Double.pi*pow(r,2))
+//let vol = round(4*Double.pi*pow(r, 3)/3)
+//print("area is", area,", volume is :", vol)
