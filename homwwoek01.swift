50518e67044b6db49a1443b76f0c95905d7255db
abdullah <shaeen364@gmail.com>
2021-10-12 08:59:32 +0300

    Abdullah mohammed HW

---
+++ homwwoek01.swift
@@ -0,0 +1,17 @@
+//
+//  homwwoek01.swift
+//  Hom01d2
+//
+//  Created by MacBook Pro on 10/12/21.
+//
+
+import Foundation
+
+//print("Enter a radius:")
+//
+//let r = Utils.readDouble()
+//let area  = round  (Double.pi * pow(r, 2))
+//let volume = round(4 * Double.pi * pow(r, 3) / 3)
+//
+//print("area is :", area, "and volume is:", volume)
+//
