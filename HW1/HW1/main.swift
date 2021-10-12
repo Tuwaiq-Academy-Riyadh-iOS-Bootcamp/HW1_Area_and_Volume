c097f5da49294c1e5c2f674a545ad06612339adf
Nora8080 <89574722+Nora8080@users.noreply.github.com>
2021-10-12 09:03:01 +0300

    hw done

---
+++ HW1/HW1/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  HW1
+//
+//  Created by نورا on 06/03/1443 AH.
+//
+
+import Foundation
+
+print("Enter a radius:", terminator: "")
+let radius = Utils.readDouble()
+
+
+
+ let A = pow(radius, 2)
+ let area = round(.pi * A)
+
+ let V = pow(radius, 3)
+ let Volume = round( (4 * .pi * V) / 3)
+
+    
+
+    print("Area is: \(area) and Volume is: \(Volume)" )
+
