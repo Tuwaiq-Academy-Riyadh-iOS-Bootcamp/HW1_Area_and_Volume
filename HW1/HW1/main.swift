2f45405d6669f21290075adacfa3bb0469dc33e5
Areej Mohammad <areejmohammad@Areejs-MacBook-Air.local>
2021-10-12 09:05:20 +0300

    AreejMashabi

---
+++ HW1/HW1/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  HW1
+//
+//  Created by Areej Mohammad on 05/03/1443 AH.
+//
+
+import Foundation
+
+print("Enter a radius:", terminator: "")
+let radiuse = Utils.readDouble()
+let pi = 3.14
+var circleArea = round(Double.pi * pow(radiuse, 2))
+
+let volume: Double = round(4 * (Double.pi * pow(radiuse, 3) / 3))
+print("area is: ", circleArea, "And volume is :" , volume )
+
